95e0c4a00913eed8a2e99572796080cde0c16039
Rey Benedicto <reybenedicto06@gmail.com>
2023-11-19 11:05:16 +0800

    Fix margin/padding issues in the Chat bubbles width; targetSdk 33 -> 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.workoutaiassistant"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.workoutaiassistant"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
