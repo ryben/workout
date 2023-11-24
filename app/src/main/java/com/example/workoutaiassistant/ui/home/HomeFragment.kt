d3b6f379ba766117d7cd90079570a1cfdca44b12
Rey Benedicto <reybenedicto06@gmail.com>
2023-11-25 00:14:23 +0800

    Moved the chat interface into a bottomsheet

--- app/src/main/java/com/example/workoutaiassistant/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/workoutaiassistant/ui/home/HomeFragment.kt
@@ -4,37 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.workoutaiassistant.R
 import com.example.workoutaiassistant.data.network.Role
 import com.example.workoutaiassistant.databinding.FragmentHomeBinding
 import com.example.workoutaiassistant.util.Util
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-
-    private val homeViewModel: HomeViewModel by lazy {
-        ViewModelProvider(this).get(HomeViewModel::class.java)
-    }
+    private val homeViewModel: HomeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textDay
-        homeViewModel.textDay.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
 
         val recyclerView = binding.rvChat
         recyclerView.layoutManager = LinearLayoutManager(context)
@@ -55,11 +44,31 @@ class HomeFragment : Fragment() {
             }
         }
 
-        return root
+        return binding.root
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
+
+        // Configure the initial and expanded states
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        bottomSheetBehavior.peekHeight = 200 // Set your desired peek height
+
+        // Optional: Listen for state changes
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                // React to state change
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                // React to sliding
+            }
+        })
+
 
         // Provide initial instructions
         homeViewModel.sendSystemMessage(
